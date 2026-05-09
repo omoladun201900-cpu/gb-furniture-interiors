0fc9edfac5b282ca7f25c11b92421d160ae923c5
omoladun201900-cpu <omoladun201900@gmail.com>
2026-05-09 18:16:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
+emailjs.init("fJMqGK_u5zRy0zupi");
+
 function submitForm() {
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var phone = document.getElementById("phone").value;
+  var message = document.getElementById("message").value;
 
-  // GET INPUT VALUES
-  let name = document.getElementById("name").value;
-  let email = document.getElementById("email").value;
-  let phone = document.getElementById("phone").value;
-  let message = document.getElementById("message").value;
-
-  // CHECK IF FIELDS ARE EMPTY
-  if (
-    name === "" ||
-    email === "" ||
-    phone === "" ||
-    message === ""
-  ) {
-
-    alert("Please complete all fields before submitting.");
-
-  } else {
-
-    alert(
-      "Thank you, " +
-      name +
-      "! Your message has been received successfully. We will contact you shortly."
-    );
-
+  if (name === "" || email === "" || phone === "" || message === "") {
+    alert("Please fill in all fields");
+    return;
   }
+
+  var templateParams = {
+    name: name,
+    email: email,
+    phone: phone,
+    message: message
+  };
+
+  emailjs.send("service_ynsvu05", "template_4dvadty", templateParams)
+    .then(function() {
+      alert("Thank you " + name + "! Your message has been sent!");
+    }, function(error) {
+      alert("Oops! Something went wrong. Please try again.");
+    });
 }
